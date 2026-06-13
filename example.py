9c71072fef636022edebe2684fab615ebd8eeb53
Kusum Kumari <kusumkumariguraru@gmail.com>
2026-06-13 23:29:18 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,15 @@
+"""import matplotlib.pyplot as plt
+players=["rohit","kohli","rahul","tilak","Surya"]
+goals=[5,9,7,6,8]
+plt.bar(players,goals)
+plt.title("WOrld cup")
+plt.xlabel("players")
+plt.ylabel("goals")
+plt.show()
+"""
+import matplotlib.pyplot as plt
+matches=[1,2,3,4,5]
+goals=[5,6,7,9,8]
+plt.plot(matches,goals)
+plt.pie([40,50,52],labels=["rohit","kohli","tilak"])
+plt.show()
