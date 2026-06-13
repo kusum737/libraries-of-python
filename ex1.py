cfaf07790eb0a44549181d81486a3ebf3c4533d7
Kusum Kumari <kusumkumariguraru@gmail.com>
2026-06-13 23:28:53 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,18 @@
+"""import numpy as np
+numbers=np.array([1,2,3,4,5])
+print(numbers*2)
+print(type(numbers))
+
+a=np.array([[1,2,3],[4,5,6]])
+
+print(a[0][1])
+print(a.shape)"""
+
+
+
+import numpy as np
+marks=np.array([80,60,40,74,63])
+print("Total",np.sum(marks))
+print("Average",np.mean(marks))
+print("Highest",np.max(marks))
+print("Lowest",np.min(marks))
---
+++ task2.py
@@ -0,0 +1,5 @@
+import numpy as np
+arr=np.array([5,10,15,20,25])
+print(arr[0])
+print(arr[4])
+print(arr[2])
